3109979e9cd1f27bd77ba7fba582f40573248b30
Владимир <vn4112807@gmail.com>
2024-12-03 23:43:19 +0700

    Добавьте файлы проекта.

---
+++ ApplicationContext.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+
+namespace пробник
+{
+     public class ApplicationContext : DbContext
+    {
+        public DbSet<BD> BD { get; set; } = null!;
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlite("Data Source=Calendar.db");
+        }
+    }
+}
---
+++ BD.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace пробник
+{
+     public class BD
+    {
+        public int ID { get; set; }
+        public string? Content { get; set; }
+    }
+}
